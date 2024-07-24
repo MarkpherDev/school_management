e7085c34b5e3ecb397ffb973e5be4428e975b7d1
MartinPC <MartinChrisPC@hotmail.com>
2024-07-24 11:00:28 -0500

    feat(service): Implementando la interfaz Service
    - School

--- src/main/java/com/martin/projects/dto/request/SaveSchool.java
+++ src/main/java/com/martin/projects/dto/request/SaveSchool.java
@@ -6,12 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SaveSchool {
 
   @NotBlank(message = "El campo name no puede ir vacio")
--- src/main/java/com/martin/projects/service/SchoolService.java
+++ src/main/java/com/martin/projects/service/SchoolService.java
@@ -1,5 +1,29 @@
 package com.martin.projects.service;
 
+import com.martin.projects.dto.request.SaveStudent;
+import com.martin.projects.dto.response.StudentDto;
+import com.martin.projects.util.StudentGender;
+import java.util.List;
+
 public interface SchoolService {
 
+  List<StudentDto> findAll();
+
+  List<StudentDto> findByName(String name);
+
+  List<StudentDto> findByLastname(String lastname);
+
+  List<StudentDto> findByGrade(int grade);
+
+  List<StudentDto> findByGender(StudentGender gender);
+
+  List<StudentDto> findBySchool(Long id);
+
+  StudentDto findById(Long id);
+
+  StudentDto createStudent(SaveStudent student);
+
+  StudentDto updateStudent(Long id, SaveStudent student);
+
+  void deleteStudent(Long id);
 }
